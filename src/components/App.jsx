2f2f891e9368b750845e4ca1985543b0699daa53
Illia-Illia <Takoexerrorx@gmail.com>
2022-10-10 10:59:55 +0300

    add request and notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,28 @@
-export const App = () => {
-  return <div>find image</div>;
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { GlobalStyle } from './GlobalStyles/globalStyles';
+import { Wrap } from './App.styled';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    imageRequest: '',
+  };
+  onFormSubmit = imageRequest => {
+    this.setState({ imageRequest });
+  };
+  render() {
+    return (
+      <Wrap>
+        <GlobalStyle />
+        <Searchbar onSubmit={this.onFormSubmit} />
+        <ImageGallery />
+        <ToastContainer autoClose={3000} />
+      </Wrap>
+    );
+  }
+}
---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import { ImagesList } from './ImageGallery.styled';
+
+export class ImageGallery extends Component {
+  render() {
+    return (
+      <ImagesList>
+        <li>
+          <img src="" alt="" />
+        </li>
+      </ImagesList>
+    );
+  }
+}
